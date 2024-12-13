75596169931f59c2c52f260016f147c142fb3747
Khashayar Barooti <khashayar.baroot@epfl.ch>
2024-12-13 14:35:30 +0000

    wasm_pain

---
+++ oracles/nodejs/oracle.js
@@ -0,0 +1,11 @@
+let imports = {};
+let wasm;
+
+const path = require('path').join(__dirname, 'oracle_bg.wasm');
+const bytes = require('fs').readFileSync(path);
+
+const wasmModule = new WebAssembly.Module(bytes);
+const wasmInstance = new WebAssembly.Instance(wasmModule, imports);
+wasm = wasmInstance.exports;
+module.exports.__wasm = wasm;
+
